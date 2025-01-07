3fe5c706a214e1d354a7395e5258f6f119e00cb2
Igor <80968651+webdevbiv@users.noreply.github.com>
2025-01-07 20:49:37 +0200

    ADD | random color function

--- 15-javascript-projects/app.js
+++ 15-javascript-projects/app.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const randomColor = getRandomColor();
+color.textContent = randomColor;
+document.body.style.backgroundColor = randomColor;
+
+btn.addEventListener("click", function () {
+  const randomColor = getRandomColor();
+  console.log(randomColor);
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
