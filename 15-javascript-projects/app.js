18b73928615c04b77598afa7beaf09db4c20147b
Igor <80968651+webdevbiv@users.noreply.github.com>
2025-01-08 19:56:51 +0200

    UPD | Logic

--- 15-javascript-projects/app.js
+++ 15-javascript-projects/app.js
@@ -1,18 +1,21 @@
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const letters = "0123456789ABCDEF";
 const randomColor = getRandomColor();
-color.textContent = randomColor;
-document.body.style.backgroundColor = randomColor;
+
+backgroundColor(randomColor);
+
+function backgroundColor(randomColor) {
+  color.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+}
 
 btn.addEventListener("click", function () {
   const randomColor = getRandomColor();
-  console.log(randomColor);
-  document.body.style.backgroundColor = randomColor;
-  color.textContent = randomColor;
+  backgroundColor(randomColor);
 });
 
 function getRandomColor() {
-  const letters = "0123456789ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
